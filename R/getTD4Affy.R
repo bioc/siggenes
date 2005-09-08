87bf6bc48a459d9f586430698a9384bdfc11dc68
Holger Schwender <holger.schwender@uni-dortmund.de>
2005-09-08 13:56:24 +0000

    Removed file/folder
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/siggenes@13996 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getTD4Affy.R
+++
@@ -1,9 +0,0 @@
-getTD4Affy<-function(genenames,cdfname){
-	tmp<-paste("https://www.affymetrix.com/analysis/netaffx/fullrecord.affx?pk=",
-		cdfname,":",genenames,sep="")
-	td<-paste("<TD><A HREF=\"",tmp,"\">",genenames,"</A></TD>",sep="")
-	td
-}
-
-
-
