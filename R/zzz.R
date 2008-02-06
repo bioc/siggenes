4d22f079d296806c36222342e37b87c05f938fa6
Holger Schwender <holger.schwender@uni-dortmund.de>
2008-02-06 18:34:34 +0000

    Updated vignette.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/siggenes@29862 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,11 @@
 }
 
 .onAttach <- function(libname, pkgname) {
-	message("\n","Latest major changes in siggenes:","\n",
-		"Version 1.9.27: EBAM update completed. Added new vignette.","\n",
-		"Version 1.11.7: Improved version of EBAM for categorical data.","\n",
-		"Version 1.11.9: Added findDelta for determining the number of genes called\n",
-                "                differentially expressed for a given FDR, and vice versa.", 
-		"\n")
-    	if(interactive() && .Platform$OS.type == "windows" && .Platform$GUI ==  "Rgui")
+    	if(interactive() && .Platform$OS.type == "windows" && .Platform$GUI ==  "Rgui"){
         	addVigs2WinMenu("siggenes")
+		winMenuAddItem("Vignettes/siggenes","Rnews Paper",
+			"openPDF(file.path(.find.package('siggenes'),'doc','siggenesRnews.pdf'))")
+	}
 }
 
 
