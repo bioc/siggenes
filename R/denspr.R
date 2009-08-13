a803e6b6ab81125be5cb3ff7dd3f346abf5abf96
Holger Schwender <holger.schwender@uni-dortmund.de>
2009-08-13 23:14:29 +0000

    Added a class denspr so that the output of denspr can be plotted easier.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/siggenes@41176 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/denspr.R
+++ R/denspr.R
@@ -1,4 +1,5 @@
-denspr<-function(x,n.interval=NULL,df=5,knots.mode=TRUE,type.nclass=c("wand","scott","FD")){
+denspr<-function(x,n.interval=NULL,df=5,knots.mode=TRUE,type.nclass=c("wand","scott","FD"),
+		addx=FALSE){
 	require(splines)
 	if(is.null(n.interval)){
 		type<-match.arg(type.nclass)
@@ -31,7 +32,27 @@ denspr<-function(x,n.interval=NULL,df=5,knots.mode=TRUE,type.nclass=c("wand","sc
 	newx<-predict(ns.out,x)
 	class(newx)<-"matrix"
 	preds<-predict(glm.out,data.frame(newx),type="response")
-	return(list(y=preds/scale,center=valHist$mids,counts=counts,
-		x.mode=x.mode,ns.out=ns.out,type=type))
+	out <- list(y=preds/scale, center=valHist$mids, counts=counts, x.mode=x.mode,
+		ns.out=ns.out, type=type, x=if(addx) x else NULL)
+	class(out) <- "denspr"
+	out
+}
+
+
+plot.denspr <- function(x, ylab="Density", xlab="x", type="l", ...){
+	if(is.null(x$x))
+		stop("If the density should be plotted, addx must be set to TRUE in denspr.")
+	xval <- unique(x$x)
+	y <- unique(x$y)
+	plot(xval[order(xval)], y[order(xval)], type=type, ylab=ylab, xlab=xlab, ...)
+}
+
+
+lines.denspr <- function(x, type="l", ...){
+	if(is.null(x$x))
+		stop("If the density should be plotted, addx must be set to TRUE in denspr.")
+	xval <- unique(x$x)
+	y <- unique(x$y)
+	lines(xval[order(xval)], y[order(xval)], type=type, ...)
 }
 
