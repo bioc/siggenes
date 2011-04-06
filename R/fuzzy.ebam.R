42f2920c8fdb70fcfbe6ee862791afe976e90be0
Holger Schwender <holger.schwender@uni-dortmund.de>
2011-04-06 23:46:35 +0000

    Added one method for SAM and EBAM.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/siggenes@54571 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/fuzzy.ebam.R
@@ -0,0 +1,128 @@
+fuzzy.ebam <- function(data, cl, type=c("asymptotic", "permutation", "abf"), W=NULL,
+		logbase=exp(1), addOne=TRUE, df.ratio=NULL, n.interval=NULL, df.dens=5, 
+		knots.mode=TRUE, type.nclass=c("FD","wand", "scott"), fast=FALSE,
+		B=100, B.more=0.1, B.max=30000, n.subset=10, rand=NA){
+	require(scrime, quietly=TRUE)
+	if(is.data.frame(data))
+		data <- as.matrix(data)
+	if(!is.matrix(data))
+		stop("data must be either a matrix or a data frame.")
+	if(!is.na(logbase) && logbase <= 1)
+		stop("logbase must be larger than 1.")
+	if(!is.logical(addOne))
+		stop("addOne must be a logic value.")
+	type <- match.arg(type)
+	type.nclass <- match.arg(type.nclass)
+	out <- rowTrendFuzzy(y=cl, mat.fuzzy=data)
+	msg <- paste("EBAM for Fuzzy Genotype Calls", 
+		if(type=="abf") "(Based on Approximate Bayes Factors)", "\n\n")
+	if(type=="asymptotic"){
+		z.dens <- denspr(out$stat, n.interval=n.interval, df=df.dens,
+			knots.mode=knots.mode, type.nclass=type.nclass)$y
+		vec.pos <- nrow(data) * out$rawp/2
+		z.null <- dnorm(out$stat)
+		return(list(z=out$stat, ratio=z.null/z.dens, vec.pos=vec.pos, vec.neg=vec.pos,
+			msg=msg))
+	}
+	if(type=="abf"){
+		if(is.null(W)){
+			warning("Since W is missing, it is set to (log(2)/qnorm(0.95))^2.")
+			W <- (log(2) / qnorm(0.95))^2
+		}
+		z <- abf(out$theta, out$varTheta, W=W, numerator=1)
+		if(!is.na(logbase))
+			z <- log(z + addOne, base=logbase)
+		sd1 <- sqrt(out$varTheta + W)
+	}
+	else{
+		z <- out$stat
+		sd1 <- NULL
+	}
+	if(is.null(df.ratio))
+		df.ratio <- ifelse(type=="abf", 3, 5)
+	if(is.null(n.interval))
+		n.interval <- 139
+	if(n.interval < 20)
+		stop("n.interval should be at least 20.")
+	succ.out <- getSuccesses(z, n.interval=n.interval)
+	mat.samp <- setup.mat.samp(cl, "t", B=B, B.more=B.more, B.max=B.max, rand=rand)
+	B <- nrow(mat.samp)
+	fail.out <- compFailFuzzy(data, mat.samp, z, sqrt(out$varTheta), succ.out$interval,
+		n.subset=n.subset, fast=fast, sd1=sd1, logbase=logbase, add1=addOne)
+	ratio <- compRatio(succ.out$center, succ.out$success, fail.out$vec.fail,
+		df=df.ratio, z=z)$ratio
+	if(fast)
+		return(list(z=z, ratio=ratio, success=succ.out$success, failure=fail.out$vec.fail,
+			center=succ.out$center, mat.samp=mat.samp, msg=msg))
+	structure(list(z=z, ratio=ratio, vec.pos=fail.out$vec.pos/B, vec.neg=fail.out$vec.neg/B,
+		mat.samp=mat.samp, msg=msg))
+}
+
+
+compFailFuzzy <- function(X, mat.samp, z, s, interval, n.subset=10, fast=FALSE, sd1=NULL, logbase=exp(1),
+		add1=TRUE){
+	if(!is.null(sd1))
+		return(compFailABF(X, mat.samp, z, s, sd1, interval, n.subset=n.subset, fast=fast, 
+			logbase=logbase, add1=add1))
+	B <- nrow(mat.samp)
+	seq.samp <- unique(c(seq(1, B, ceiling(B/n.subset)), B+1))
+	n.int <- length(interval) - 1
+	n.seq <- length(seq.samp) - 1
+	vec.fail <- numeric(n.int)
+	n.row <- nrow(X)
+	z.range <- range(z)
+	mat.samp <- mat.samp - mean(mat.samp[1,])
+	if(!fast){
+		vec.pos <- vec.neg <- numeric(n.row)
+		z.rank <- rank(-abs(z), ties="first")
+	}
+	else
+		vec.pos <- vec.neg <- NULL
+	for(i in 1:n.seq){
+		tmpmat <- t(mat.samp[seq.samp[i]:(seq.samp[i+1] - 1),, drop=FALSE])
+		z.perm <- as.vector(X %*% tmpmat * s)
+		tmp <- getFailure(z.perm, z, interval, z.range=z.range, n.interval=n.int)
+		vec.fail <- vec.fail + tmp
+		if(!fast){
+			tmp <- compFalse(z, z.perm, z.rank, n.row)
+			vec.pos <- vec.pos + tmp$vec.pos
+			vec.neg <- vec.neg + tmp$vec.neg
+		}
+	}
+	structure(list(vec.fail=vec.fail, vec.pos=vec.pos, vec.neg=vec.neg))
+}
+
+
+compFailABF <- function(X, mat.samp, z, s, sd1, interval, n.subset=10, fast=FALSE, logbase=exp(1), add1=TRUE){
+	B <- nrow(mat.samp)
+	seq.samp <- unique(c(seq(1, B, ceiling(B/n.subset)), B+1))
+	n.int <- length(interval) - 1
+	n.seq <- length(seq.samp) - 1
+	n.row <- nrow(X)
+	mat.samp <- mat.samp - mean(mat.samp[1,])
+	z.range <- range(z)
+	var0 <- s*s
+	vec.fail <- numeric(n.int)
+	if(!fast){
+		vec.neg <- numeric(n.row)
+		vec.pos <- -n.seq * rank(-z, ties="first")
+	}
+	else
+		vec.pos <- vec.neg <- NULL
+	for(i in 1:n.seq){
+		tmpvec <- as.vector(X %*% t(mat.samp[seq.samp[i]:(seq.samp[i+1] - 1),, drop=FALSE]) * var0)
+		z.perm <- dnorm(tmpvec, sd=sd1) / dnorm(tmpvec, sd=s)
+		if(!is.na(logbase))
+			z.perm <- log(z.perm + add1, base=logbase)
+		tmp <- getFailure(z.perm, z, interval, z.range=z.range, n.interval=n.int)
+		vec.fail <- vec.fail + tmp
+		if(!fast)
+			vec.pos <- vec.pos + rank(-c(z.perm, z), ties="first")[length(z.perm) + (1:n.row)]
+	}
+	structure(list(vec.fail=vec.fail, vec.pos=vec.pos, vec.neg=vec.neg))
+}
+
+
+
+
+		
---
+++ R/fuzzy.stat.R
@@ -0,0 +1,111 @@
+fuzzy.stat <- function(data, cl, type=c("asymptotic", "permutation", "abf"), W=NULL, logbase=exp(1),
+		addOne=TRUE, B=100, B.more=0.1, B.max=30000, n.subset=10, rand=NA){
+	require(scrime, quietly=TRUE)
+	if(is.data.frame(data))
+		data <- as.matrix(data)
+	if(!is.matrix(data))
+		stop("data must be either a matrix or a data frame.")
+	if(!is.na(logbase) && logbase <= 1)
+		stop("logbase must be larger than 1.")
+	out <- rowTrendFuzzy(y=cl, mat.fuzzy=data)
+	type <- match.arg(type)
+	if(type=="abf"){
+		if(is.null(W)){
+			warning("Since W is missing, it is set to (log(2)/qnorm(0.95))^2.")
+			W <- (log(2) / qnorm(0.95))^2
+		}
+		z <- abf(out$theta, out$varTheta, W=W, numerator=1)
+		if(!is.na(logbase))
+			z <- log(z+addOne, base=logbase)
+		sd1 <- sqrt(out$varTheta + W)
+	}
+	else{
+		z <- out$stat
+		sd1 <- NULL
+	}
+	s <- sqrt(out$varTheta)
+	if(type=="asymptotic"){
+		null.out <- fuzzy.null.approx(out$rawp)
+		mat.samp <- matrix(numeric(0))
+	}
+	else{
+		mat.samp <- setup.mat.samp(cl, "t", B=B, B.more=B.more, B.max=B.max, rand=rand)
+		null.out <- fuzzy.null(data, mat.samp, z, s, sd1, n.subset=n.subset, logbase=logbase, add1=addOne)	 
+	}
+	msg <- paste("SAM for Fuzzy Genotype Calls", if(type=="abf") "(Based on Approximate Bayes Factors)", "\n\n")
+	list(d=z, d.bar=null.out$d.bar, p.value=null.out$p.value, vec.false=null.out$vec.false, 
+		s=s, s0=numeric(0), mat.samp=mat.samp, msg=msg, fold=numeric(0))
+}
+
+fuzzy.null.approx <- function(rawp){
+	m <- length(na.exclude(rawp))
+	d.bar <- qnorm(((1:m) - 0.5) / m)
+	vec.false <- m * rawp / 2
+	list(d.bar=d.bar, p.value=rawp, vec.false=vec.false)
+}
+
+fuzzy.null <- function(mat.fuzzy, mat.samp, d, s, sd1, n.subset=10, logbase=exp(1), add1=TRUE){
+	if(!is.null(sd1))
+		return(fuzzy.null.abf(mat.fuzzy, mat.samp, d, s, sd1, n.subset=n.subset, logbase=logbase, add1=add1))
+	B <- nrow(mat.samp)
+	vecB <- unique(c(seq(1, B, n.subset), B+1))
+	n.int <- length(vecB) - 1
+	n.var <- nrow(mat.fuzzy)
+	mat.samp <- mat.samp - mean(mat.samp[1,])
+	dpos <- d >= 0
+	d <- abs(d)
+	d.rank <- rank(-d, ties="first")
+	vec.dperm <- numeric(n.var)
+	vec.pos <- vec.neg <- -n.int * d.rank
+	for(i in 1:n.int){
+		tmpmat <- t(mat.samp[vecB[i]:(vecB[i+1] - 1),, drop=FALSE])
+		mat.dperm <- mat.fuzzy %*% tmpmat * s
+		mat.dperm <- apply(mat.dperm, 2, sort)
+		vec.dperm <- vec.dperm + rowSums(mat.dperm, na.rm=TRUE)
+		pos <- mat.dperm >= 0
+		tmp <- rank(-c(mat.dperm[pos], d), na.last=NA, ties="first")
+		vec.pos <- vec.pos + tmp[length(tmp) - ((n.var-1):0)] 
+		tmp <- rank(c(mat.dperm[!pos], -d), na.last=NA, ties="first")
+		vec.neg <- vec.neg + tmp[length(tmp) - ((n.var-1):0)] 
+	}
+	d.bar <- vec.dperm / B
+	p.value <- (vec.pos + vec.neg) / (n.var * B)
+	vec.false <- numeric(n.var)
+	vec.false[dpos] <- vec.pos[dpos]
+	vec.false[!dpos] <- vec.neg[!dpos]
+	structure(list(d.bar=d.bar, vec.false=vec.false/B, p.value=p.value))
+}
+		
+fuzzy.null.abf <- function(mat.fuzzy, mat.samp, d, s, sd1, n.subset=10, logbase=exp(1), add1=TRUE){
+	B <- nrow(mat.samp)
+	vecB <- unique(c(seq(1, B, n.subset), B+1))
+	n.int <- length(vecB) - 1
+	n.var <- nrow(mat.fuzzy)
+	mat.samp <- mat.samp - mean(mat.samp[1,])
+	vec.dperm <- numeric(n.var)
+	d.rank <- rank(-d, ties="first")
+	vec.false <- -n.int * d.rank
+	var0 <- s * s
+	for(i in 1:n.int){
+		tmpmat <- mat.fuzzy %*% t(mat.samp[vecB[i]:(vecB[i+1] - 1),, drop=FALSE]) * var0 
+		mat.dperm <- dnorm(tmpmat, sd=sd1) / dnorm(tmpmat, sd=s)
+		if(!is.na(logbase))
+			mat.dperm <- log(mat.dperm + add1, base=logbase)
+		mat.dperm <- apply(mat.dperm, 2, sort)
+		vec.dperm <- vec.dperm + rowSums(mat.dperm)
+		tmp <- rank(-c(as.vector(mat.dperm), d), ties="first")
+		vec.false <- vec.false + tmp[length(tmp)-((n.var-1):0)]
+	}
+	d.bar <- vec.dperm / B
+	vec.false <- vec.false / B
+	p.value <- vec.false / n.var
+	structure(list(d.bar=d.bar, vec.false=vec.false, p.value=p.value))
+}
+
+
+		
+
+
+
+
+
